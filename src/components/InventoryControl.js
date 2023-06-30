e6cad08a11c1ba3b96198865ca434a127cac0d5c
Sarah Reimann <reimannsarah45@gmail.com>
2023-06-30 15:01:19 -0700

    update terminology

--- src/components/InventoryControl.js
+++ src/components/InventoryControl.js
@@ -3,6 +3,7 @@ import Inventory from "./Inventory";
 import InventoryForm from "./InventoryForm";
 import NavBar from './NavBar';
 import InventoryDetail from "./InventoryDetail";
+import EditInventoryForm from "./EditInventoryForm";
 
 class InventoryControl extends React.Component {
 
@@ -11,15 +12,16 @@ class InventoryControl extends React.Component {
     this.state = {
       inventoryFormVisibleOnPage: false,
       inventory: [],
-      selectedItem: null
+      selectedPlant: null,
+      editFormVisibleOnPage: false
     }
   }
 
 
   handleClick = () => {
-    if (this.state.currentItem != null) {
+    if (this.state.currentPlant != null) {
       this.setState({inventoryFormVisibleOnPage: false,
-                      currentItem: null
+                      currentPlant: null
                     });
     }
     this.setState((prevState => ({
@@ -31,30 +33,72 @@ class InventoryControl extends React.Component {
     this.setState({inventoryFormVisibleOnPage: false})
   }
 
-  handleChangingSelectedItem = (id) => {
-    const currentItem = this.state.inventory.filter(ticket => ticket.id === id)[0];
-    this.setState({selectedItem: currentItem});
+  handleChangingSelectedPlant = (id) => {
+    const currentPlant = this.state.inventory.filter(plant => plant.id === id)[0];
+    this.setState({selectedPlant: currentPlant});
   }
 
-  handleAddingToInventory = (newItem) => {
-    const newInventoryList = this.state.inventory.concat(newItem);
+  handleAddingToInventory = (newPlant) => {
+    const newInventoryList = this.state.inventory.concat(newPlant);
     console.log(newInventoryList);
     this.setState({ inventory: newInventoryList,
                     inventoryFormVisibleOnPage: false});
+  }
+
+  handleDeleteFromInventory = (id) => {
+    const newInventoryList = this.state.inventory.filter(plant => plant.id !== id);
+    this.setState({inventory: newInventoryList,
+                    selectedPlant: null,
+                    inventoryFormVisibleOnPage: false});
+  }
+
+  handleEditClick = () => {
+    this.setState({editFormVisibleOnPage: true});
+  }  
+
+  handleSellingPlantClick = (id) => {
+    const updatedInventory = this.state.inventory.map((plant) => {
+      if (plant.id === id) {
+        return {
+          ...plant,
+          plantQuantity: plant.plantQuantity - 1
+        };
+      }
+      return plant;
+    });
+  
+    this.setState({
+      inventory: updatedInventory
+    });
+    console.log(this.state.inventory[id])
+  }
+  
+
+  handleEditingInventoryPlant = (plantToEdit) => {
+    const editedInventory = this.state.inventory.filter(plant => plant.id !== this.state.selectedPlant.id).concat(plantToEdit);
+    this.setState({
+      inventory: editedInventory,
+      editFormVisibleOnPage: false,
+      selectedPlant: null
+    })
   }
 
   render() {
     let currentlyVisibleState = null;
     let buttonText = null;
     
-    if (this.state.selectedItem != null) {
-      currentlyVisibleState = <InventoryDetail item={this.state.selectedItem} />
+    if (this.state.selectedPlant != null) {
+      currentlyVisibleState = <InventoryDetail plant={this.state.selectedPlant} onDeleteClick={this.handleDeleteFromInventory} onEditClick={this.handleEditClick} onSellClick={this.handleSellingPlantClick}/>
+      buttonText="Back";
     } else if (this.state.inventoryFormVisibleOnPage) {
       currentlyVisibleState = <InventoryForm onNewInventoryCreation={this.handleAddingToInventory} />
       buttonText="Back";
+    } else if (this.state.editFormVisibleOnPage) {
+      currentlyVisibleState = <EditInventoryForm plant={this.state.selectedPlant} onEditSubmission={this.handleEditingInventoryPlant} />
+      buttonText="Back";
     } else {
-      currentlyVisibleState = <Inventory inventory={this.state.inventory} onItemSelect={this.handleChangingSelectedItem}/>
-      buttonText="Add item to inventory"
+      currentlyVisibleState = <Inventory inventory={this.state.inventory} onPlantSelect={this.handleChangingSelectedPlant}/>
+      buttonText="Add plant to inventory"
     }
 
     return (
--- src/components/InventoryDetail.js
+++ src/components/InventoryDetail.js
@@ -1,18 +1,26 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 function InventoryDetail(props) {
-  const { item } = props;
+  const { plant, onDeleteClick, onEditClick, onSellClick } = props;
   return (
     <React.Fragment>
-      <h1>Item Details</h1>
-      <h3>{item.colloquial}</h3>
-      <p>Latin Name: {item.name}</p>
-      <p>Price Per Pallet: {item.pricePerPallet}</p>
-      <p>Price Per Plant: {item.pricePerPlant}</p>
-      <p>Quantity of Pallets in Stock: {item.palletQuantity}</p>
-      <p>Quantity of Plants in Stock: {item.plantQuantity}</p>
+      <h1>Plant Details</h1>
+      <h3>{plant.colloquial}</h3>
+      <p>Latin Name: {plant.name}</p>
+      <p>Quantity of Pallets in Stock: {plant.palletQuantity}</p>
+      <p>Quantity of Plants in Stock: {plant.plantQuantity}</p>
+      <button onClick={() => onDeleteClick(plant.id)}>Delete</button>
+      <button onClick={onEditClick}>Edit</button>
+      <button onClick={onSellClick}>Sell Plant</button>
     </React.Fragment>
   );
+}
+
+InventoryDetail.propTypes = {
+  plant: PropTypes.object,
+  onDeleteClick: PropTypes.func,
+  onEditClick: PropTypes.func
 }
 
 export default InventoryDetail;
