57714034a8f0fa1b117edf72e2c477adfec11c92
Sarah Reimann <reimannsarah45@gmail.com>
2023-06-30 10:27:18 -0700

    change inventory component to inventoryControl

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import Inventory from './Inventory';
+import InventoryControl from './InventoryControl';
 import Header from './Header';
 
 function App() {
   return (
     <React.Fragment>
       <Header />
-      <Inventory />
+      <InventoryControl />
     </React.Fragment>
   );
 }
